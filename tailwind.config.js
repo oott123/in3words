8d4784dfa2e338c2e71d9e56ae5e8e43bc725f60
oott123 <git@public.oott123.com>
2021-12-05 17:55:29 +0800

    feat: add sass

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        gray: colors.blueGray,
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
