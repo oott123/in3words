0566ad6d7577466adeafa6f49096b298af592596
oott123 <git@public.oott123.com>
2021-12-08 19:55:33 +0800

    refactor: assets

---
+++ app/components/PageNavigation.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const PageNavigation: React.FC<{ totalPages: number; page: number }> = ({
+  totalPages,
+  page,
+}) => {
+  return <div className="PageNavigation"></div>
+}
+
+export default PageNavigation
