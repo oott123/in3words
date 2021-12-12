bcc9a793dc9ca8b2282f39938324679d515cc1ad
oott123 <git@public.oott123.com>
2021-12-12 18:48:11 +0800

    refactor: add blog card

---
+++ app/components/BlogCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const BlogCard: React.FC = ({ children }) => {
+  return <div className="BlogCard">{children}</div>
+}
+
+export default BlogCard
--- app/components/BlogPost.tsx
+++ app/components/BlogPost.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { Link } from 'remix'
 import { Post, SummarizedPost } from '~/data/posts'
 import { authorPath, categoryPath, tagPath } from '~/path'
+import BlogCard from './BlogCard'
 import BlogDate from './BlogDate'
 import BlogIcon, { BlogIcons } from './BlogIcon'
 
@@ -14,59 +15,61 @@ const BlogPost: React.FC<{
   const summarized = 'summary' in post
 
   return (
-    <article
-      className={classNames('BlogPost', summarized && 'BlogPost-Summarized')}
-    >
-      <header className="BlogPost_Head">
-        <h1 className="BlogPost_Title">
-          <Link
-            to={postPath}
-            dangerouslySetInnerHTML={{ __html: post.title }}
-          />
-        </h1>
-        <div className="BlogPost_Meta">
-          <BlogMeta label={<BlogIcon>{BlogIcons.User}</BlogIcon>}>
-            <Link to={authorPath(post.author)}>{post.author.name}</Link>
-          </BlogMeta>
-          <BlogMeta label={<BlogIcon>{BlogIcons.Date}</BlogIcon>}>
+    <BlogCard>
+      <article
+        className={classNames('BlogPost', summarized && 'BlogPost-Summarized')}
+      >
+        <header className="BlogPost_Head">
+          <h1 className="BlogPost_Title">
+            <Link
+              to={postPath}
+              dangerouslySetInnerHTML={{ __html: post.title }}
+            />
+          </h1>
+          <div className="BlogPost_Meta">
+            <BlogMeta label={<BlogIcon>{BlogIcons.User}</BlogIcon>}>
+              <Link to={authorPath(post.author)}>{post.author.name}</Link>
+            </BlogMeta>
+            <BlogMeta label={<BlogIcon>{BlogIcons.Date}</BlogIcon>}>
+              <Link to={postPath}>
+                <BlogDate date={post.createdAt} />
+              </Link>
+            </BlogMeta>
+            {!!post.categories?.length && (
+              <BlogMeta label={<BlogIcon>{BlogIcons.Category}</BlogIcon>}>
+                {post.categories.map((c) => (
+                  <Link to={categoryPath(c)} key={c.slug}>
+                    {c.name}
+                  </Link>
+                ))}
+              </BlogMeta>
+            )}
+            {!!post.tags?.length && (
+              <BlogMeta label={<BlogIcon>{BlogIcons.Tag}</BlogIcon>}>
+                {post.tags.map((t) => (
+                  <Link to={tagPath(t)} key={t.slug}>
+                    {t.name}
+                  </Link>
+                ))}
+              </BlogMeta>
+            )}
+          </div>
+        </header>
+        <section
+          className="BlogPost_Body"
+          dangerouslySetInnerHTML={{
+            __html: summarized ? post.summary : post.content,
+          }}
+        ></section>
+        {summarized && (
+          <footer className="BlogPost_ReadMore">
             <Link to={postPath}>
-              <BlogDate date={post.createdAt} />
+              {isLoadingFull ? '全文载入中……' : '阅读全文»'}
             </Link>
-          </BlogMeta>
-          {!!post.categories?.length && (
-            <BlogMeta label={<BlogIcon>{BlogIcons.Category}</BlogIcon>}>
-              {post.categories.map((c) => (
-                <Link to={categoryPath(c)} key={c.slug}>
-                  {c.name}
-                </Link>
-              ))}
-            </BlogMeta>
-          )}
-          {!!post.tags?.length && (
-            <BlogMeta label={<BlogIcon>{BlogIcons.Tag}</BlogIcon>}>
-              {post.tags.map((t) => (
-                <Link to={tagPath(t)} key={t.slug}>
-                  {t.name}
-                </Link>
-              ))}
-            </BlogMeta>
-          )}
-        </div>
-      </header>
-      <section
-        className="BlogPost_Body"
-        dangerouslySetInnerHTML={{
-          __html: summarized ? post.summary : post.content,
-        }}
-      ></section>
-      {summarized && (
-        <footer className="BlogPost_ReadMore">
-          <Link to={postPath}>
-            {isLoadingFull ? '全文载入中……' : '阅读全文»'}
-          </Link>
-        </footer>
-      )}
-    </article>
+          </footer>
+        )}
+      </article>
+    </BlogCard>
   )
 }
 
