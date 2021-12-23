2ff6fb5bc600690206cdd492fa4a6a17216370aa
oott123 <git@public.oott123.com>
2021-12-23 13:57:59 +0800

    refactor: use gulp to build styles

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const postcss = require('gulp-postcss')
+
+function fonts() {
+  return gulp
+    .src('node_modules/@fontsource/*/files/*.{ttf,woff,woff2,eot,svg}')
+    .pipe(gulp.dest('public/build/gulp/fonts'))
+}
+
+function styles() {
+  return gulp
+    .src('styles/index.scss')
+    .pipe(
+      sass
+        .sync({
+          includePaths: ['node_modules'],
+        })
+        .on('error', sass.logError),
+    )
+    .pipe(
+      postcss({
+        plugins: [require('tailwindcss'), require('autoprefixer')],
+      }),
+    )
+    .pipe(gulp.dest('app/styles'))
+}
+
+function watchStyles() {
+  return gulp.watch('styles/**/*.scss', { ignoreInitial: false }, styles)
+}
+
+const watch = gulp.parallel([watchStyles])
+const defaultTask = gulp.parallel([fonts, watch])
+
+exports.watch = watch
+exports.styles = styles
+exports.fonts = fonts
+exports.build = gulp.parallel([fonts, styles])
+exports.default = defaultTask
