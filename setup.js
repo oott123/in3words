fcd9bebf1b9d0ac2b5f2fd2dbaeac26cebc9ecbe
oott123 <git@public.oott123.com>
2021-12-26 00:28:07 +0800

    fix: postinstall output

--- setup.js
+++ setup.js
@@ -3,5 +3,5 @@ const { spawnSync } = require('child_process')
 if (process.env.DISABLE_POST_INSTALL === 'yes') {
   process.exit(0)
 } else {
-  spawnSync('yarn', ['remix', 'setup', 'node'])
+  spawnSync('yarn', ['remix', 'setup', 'node'], { stdio: 'inherit' })
 }
