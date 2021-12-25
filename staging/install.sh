9db28087803ed7490021e0ead7151545f252b2e8
oott123 <git@public.oott123.com>
2021-12-26 01:32:56 +0800

    fix: staging deploy

--- staging/install.sh
+++ staging/install.sh
@@ -2,7 +2,7 @@
 set -xeo pipefail
 cd /var/www/in3words
 chown www-data:www-data -R .
-sudo -u www-data yarn --production --ignore-scripts
+sudo -u www-data sh -c 'DISABLE_POST_INSTALL=yes yarn workspaces focus --all --production'
 # cp /var/www/in3words/staging/in3words.service /etc/systemd/system/in3words.service
 # chown root:root /etc/systemd/system/in3words.service
 # chmod 644 /etc/systemd/system/in3words.service
--- staging/staging.sh
+++ staging/staging.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 yarn build
 source ./staging.env
-rsync -avP yarn.lock package.json build public "$I3W_REMOTE_HOST":/var/www/in3words/
+rsync --exclude '.yarn/cache' --exclude '.yarn/unplugged' --exclude '.yarn/install-state.gz' -avP yarn.lock package.json build public .yarnrc.yml .yarn setup.js "$I3W_REMOTE_HOST":/var/www/in3words/
 ssh "$I3W_REMOTE_HOST" < ./staging/install.sh
