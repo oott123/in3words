151087c10f04f9b7f4bc6f4adf80135b513f7c30
oott123 <git@public.oott123.com>
2021-12-25 20:48:44 +0800

    refactor: rename public build dir

--- remix.config.js
+++ remix.config.js
@@ -2,9 +2,9 @@
  * @type {import('@remix-run/dev/config').AppConfig}
  */
 module.exports = {
-  appDirectory: "app",
-  browserBuildDirectory: "public/build",
-  publicPath: "/build/",
-  serverBuildDirectory: "build",
-  devServerPort: 8002
-};
+  appDirectory: 'app',
+  browserBuildDirectory: 'public/_i3w_remix',
+  publicPath: '/_i3w_remix/',
+  serverBuildDirectory: 'build',
+  devServerPort: 8002,
+}
