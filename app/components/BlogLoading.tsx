62fc9feec08d939beafdfee8ad2641fc166bcda3
oott123 <git@public.oott123.com>
2021-12-25 13:19:47 +0800

    feat: fix svg

--- app/components/BlogLoading.tsx
+++ app/components/BlogLoading.tsx
@@ -26,7 +26,7 @@ export const LoadingSvg = () => (
       y="0"
       width="100%"
       height="100%"
-      clip-path="url(#clip-path)"
+      clipPath="url(#clip-path)"
       style={{ fill: 'url("#fill")' }}
     ></rect>
     <defs>
